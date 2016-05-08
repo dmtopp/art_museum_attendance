4a7119af14a648b6fd806d2959b47efd2b2dc2d6
Daniel Topp <dmtopp@gmail.com>
2016-05-08 18:18:11 -0500

    added basic server and index page

---
+++ server.js
@@ -0,0 +1,20 @@
+// set up dependencies
+require('dotenv').config()
+var express    = require('express'),
+    bodyParser = require('body-parser'),
+    app        = express();
+
+
+// configure our body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// set our route to our homepage
+app.get('/', function(req, res, next) {
+  res.sendFile(__dirname + '/views/index.html');
+})
+
+// start our server!
+var server = app.listen(process.env.PORT || 3000, function(){
+  console.log('The server is listening on port ' + server.address().port + '!');
+})
