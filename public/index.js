0ddab918d4176f4821705d243ad63797eec0a420
Daniel Topp <dmtopp@gmail.com>
2016-07-10 13:20:48 -0500

    Made image local and made it so that users must enter a name and location before proceeding.

--- public/index.js
+++ public/index.js
@@ -1,14 +1,25 @@
 window.onload = function() {
-  console.log('hello from index');
   var location = document.querySelector('#location');
   var name = document.querySelector('#name');
+  var gotIt = document.querySelector("#got-it");
+
+  var locationValue, nameValue;
+
+  gotIt.addEventListener('click', function(e) {
+    if (!locationValue || !nameValue) {
+      e.preventDefault();
+      document.querySelector('#title').innerHTML = 'Please enter a name and a location!';
+    }
+  })
 
   location.addEventListener('change', function(e) {
     sessionStorage.setItem('location', e.target.value);
+    locationValue = e.target.value;
   })
 
   name.addEventListener('change', function(e) {
     sessionStorage.setItem('name', e.target.value);
+    nameValue = e.target.value;
   })
 
 }
